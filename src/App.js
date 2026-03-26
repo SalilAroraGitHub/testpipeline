59d24171a234bdc5bb345f0ce50c5ae9b8e76bc7
salilarora <salilarora0213@gmail.com>
2026-03-26 22:37:49 +0530

    updated codd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>CI/CD LIVE WORKING 🔥🔥</h1>;
+  return (
+    <>
+      <h1>CI/CD LIVE WORKING 🔥🔥</h1>
+      <h1>hello Salil</h1>
+    </>
+  );
 }
 
 export default App;
